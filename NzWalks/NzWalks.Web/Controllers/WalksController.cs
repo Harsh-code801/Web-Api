b745266303c8b293b6cdf43dc6c1fffecd53f1b4
harsh <harsh@DESKTOP-2GQ2PKH>
2023-08-21 22:44:05 +0530

    Add Web Project with Get Call

---
+++ NzWalks/NzWalks.Web/Controllers/WalksController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NzWalks.Web.Controllers
+{
+    public class WalksController : Controller
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public WalksController(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                var client = httpClientFactory.CreateClient();
+                var httpResponseMessage = await client.GetAsync("https://localhost:7111/api/Walks");
+                httpResponseMessage.EnsureSuccessStatusCode();
+                string body = await httpResponseMessage.Content.ReadAsStringAsync();
+                ViewBag.Body = body;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return View();
+        }
+    }
+}
